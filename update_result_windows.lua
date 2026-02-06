42343ff6f3d1980ab0859ba9afb9dc46ae60d4bb
Walter Hofstetter <43199857+netwho@users.noreply.github.com>
2026-02-06 17:19:28 +0100

    Delete update_result_windows.lua

--- update_result_windows.lua
+++
@@ -1,7 +0,0 @@
--- This file documents the pattern for updating show_result_window calls
--- We need to change calls like:
---   show_result_window("DNS Registration Info: " .. domain, formatted)
--- To:
---   show_result_window_with_buttons("DNS Registration Info: " .. domain, formatted, "DNS RDAP", domain)
-
--- The pattern is: show_result_window_with_buttons(title, content, query_type, query_target)
